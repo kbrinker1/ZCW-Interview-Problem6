c85d4a44209f185ec938710e7dc1de5c5c377d32
Keith Brinker <keithbrinker@zipcoders-MBP-2.fios-router.home>
2018-04-15 15:57:38 -0400

    finished

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,9 +1,46 @@
 package io.zipcoder;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Problem6 {
 
 
-    public static String writeInMilitaryTime(String timetoConvert) {
+    public static String writeInMilitaryTime(String timeToConvert) {
 
+        String timeFind = "(1[012]|[1-9]):([0-5][0-9])(am|pm)"; //Regular Expression for parsing digital time
+
+        String[] analogTime = { "Zero Zero", "Zero One", "Zero Two", "Zero Three", "Zero Four", "Zero Five", "Zero Six", "Zero Seven", "Zero Eight", "Zero Nine",
+                                "Ten", "Eleven","Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen",
+                                "Twenty", "Twenty One", "Twenty Two", "Twenty Three", "Twenty Four", "Twenty Five", "Twenty Six", "Twenty Seven", "Twenty Eight", "Twenty Nine",
+                                "Thirty", "Thirty One", "Thirty Two", "Thirty Three", "Thirty Four", "Thirty Five", "Thirty Six", "Thirty Seven", "Thirty Eight", "Thirty Nine",
+                                "Forty", "Forty One", "Forty Two", "Forty Three", "Forty Four", "Forty Five", "Forty Six", "Forty Seven", "Forty Eight", "Forty Nine",
+                                "Fifty", "Fifty One", "Fifty Two", "Fifty Three", "Fifty Four", "Fifty Five", "Fifty Six", "Fifty Seven", "Fifty Eight", "Fifty Nine"};
+
+
+        Integer[] matchRawtoAnalog = new Integer[2];
+
+        Pattern p = Pattern.compile(timeFind);
+        Matcher m = p.matcher(timeToConvert);
+
+        if (m.find()){
+            if (m.group(3).equals("pm")){
+                matchRawtoAnalog[0] = Integer.valueOf(m.group(1)) + 12; //converts pm hours to military
+            }
+            else {
+                matchRawtoAnalog[0] = Integer.valueOf(m.group(1));      //am hours don't need converting
+            }
+            matchRawtoAnalog[1] = Integer.valueOf(m.group(2));          // takes minutes
+        }
+
+        String printAnalogTime = "";
+        if (matchRawtoAnalog[1] < 10){
+            printAnalogTime = analogTime[matchRawtoAnalog[0]] + " Hundred " + analogTime[matchRawtoAnalog[1]] + " Hours";
+        }
+        else {
+            printAnalogTime = analogTime[matchRawtoAnalog[0]] + " Hundred and " + analogTime[matchRawtoAnalog[1]] + " Hours";
+        }
+
+        return printAnalogTime;
     }
 }

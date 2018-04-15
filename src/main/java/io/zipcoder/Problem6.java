9a676b4621cf992d3a20827fbc32e7a92a39c6f0
Keith Brinker <keithbrinker@zipcoders-MBP-2.fios-router.home>
2018-04-15 15:23:44 -0400

    tests written, method stubbed out, game time

--- src/main/java/io/zipcoder/Problem6.java
+++ src/main/java/io/zipcoder/Problem6.java
@@ -1,4 +1,9 @@
 package io.zipcoder;
 
 public class Problem6 {
+
+
+    public static String writeInMilitaryTime(String timetoConvert) {
+
+    }
 }
--- src/test/java/io/zipcoder/Problem6Test.java
+++ src/test/java/io/zipcoder/Problem6Test.java
@@ -1,4 +1,72 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
 public class Problem6Test {
+
+
+    @Test
+    public void testOneThirtyPM(){
+        //given
+        String timeToConvert = "1:30pm";
+
+        //when
+        String actual = "Thirteen Hundred and Thirty Hours";
+        String expected = Problem6.writeInMilitaryTime(timeToConvert);
+        //then
+        Assert.assertEquals(actual, expected);
+    }
+
+
+
+    @Test
+    public void testOneThirtyAM(){
+        //given
+        String timeToConvert = "1:30am";
+
+        //when
+        String actual = "Zero One Hundred and Thirty Hours";
+        String expected = Problem6.writeInMilitaryTime(timeToConvert);
+        //then
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testTwoTwentyTwoPM(){
+        //given
+        String timeToConvert = "2:22pm";
+
+        //when
+        String actual = "Fourteen Hundred and Twenty Two Hours";
+        String expected = Problem6.writeInMilitaryTime(timeToConvert);
+        //then
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testTwoElevenAM(){
+        //given
+        String timeToConvert = "2:11am";
+
+        //when
+        String actual = "Zero Two Hundred and Eleven Hours";
+        String expected = Problem6.writeInMilitaryTime(timeToConvert);
+        //then
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testTenZeroTwoAM(){
+        //given
+        String timeToConvert = "10:02am";
+
+        //when
+        String actual = "Ten Hundred Zero Two Hours";
+        String expected = Problem6.writeInMilitaryTime(timeToConvert);
+        //then
+        Assert.assertEquals(actual, expected);
+    }
 }
